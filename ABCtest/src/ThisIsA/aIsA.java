dab288a889c37387425ad156b786233188a79ec0
JasonChang <kingsabaoni07@gmail.com>
2021-11-30 15:02:55 +0800

    2nd version

---
+++ ABCtest/src/ThisIsA/aIsA.java
@@ -0,0 +1,11 @@
+package ThisIsA;
+
+public class aIsA {
+	public static void main(String[] args) {
+		int x,y;
+		x=87;
+		y=87;
+		System.out.println("x+y= " + (x+y));
+		System.out.println(" 1st Version" );
+	}
+}
